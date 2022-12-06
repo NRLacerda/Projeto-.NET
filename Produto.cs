105a5af15f0531d5bbb226e4b6ecc92514a53428
Nicolas Rocha Lacerda <94977120+NRLacerda@users.noreply.github.com>
2022-12-05 22:10:35 -0300

    Adicionar arquivos de projeto.

---
+++ Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo1
+{
+    public class Produto
+    {
+        public string Nome;
+        public int Quantidade;
+        public double Preco;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Estudo1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string teste;
+            Console.WriteLine("Entre os seguintes dados do produtos.");
+            Console.WriteLine("VALOR UNIDADE TOTAL");
+            teste = Console.ReadLine();
+            Console.WriteLine(teste);
+        }
+    }
+}
